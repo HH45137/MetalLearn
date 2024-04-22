d8715a9d68354cd53ee46e18054b68748bc46c40
Carl-Y <3387640174@qq.com>
2024-04-22 16:50:18 +0800

    Initial Commit

---
+++ MetalLearn/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MetalLearn
+//
+//  Created by dcy on 2024/4/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
