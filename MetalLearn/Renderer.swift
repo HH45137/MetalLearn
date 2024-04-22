53ba4d6d5ef13188c50e65faa681cfac4e68dafe
Carl-Y <3387640174@qq.com>
2024-04-22 17:29:15 +0800

    Update Renderer.swift

--- MetalLearn/Renderer.swift
+++ MetalLearn/Renderer.swift
@@ -3,8 +3,13 @@ import MetalKit
 import simd
 
 class Renderer: NSObject, MTKViewDelegate {
+    
+    var device: MTLDevice
+    var commandQueue: MTLCommandQueue
+    
     init?(metalKitView: MTKView) {
-        
+        self.device = metalKitView.device!
+        self.commandQueue = self.device.makeCommandQueue()!
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
@@ -13,6 +18,20 @@ class Renderer: NSObject, MTKViewDelegate {
     
     func draw(in view: MTKView) {
         // Draw code
+        
+        let commandBuffer = self.commandQueue.makeCommandBuffer()
+        
+        let renderPassDescriptor = view.currentRenderPassDescriptor!
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        
+        renderEncoder?.endEncoding()
+        
+        let drawable = view.currentDrawable!
+        commandBuffer?.present(drawable)
+        
+        commandBuffer?.commit()
     }
     
 }
